64098303f856fa75fc25d9d4b23adfcba59e825f
Cristian Ares <cristianhares@outlook.com>
2020-07-02 00:13:38 +1200

    BUGFIX: Allow the MMA port in both firewalld and SELinux policy, dinamically add dependencies in ks.cfg

--- templates/template_msaz_sentinel.sh
+++ templates/template_msaz_sentinel.sh
@@ -1,3 +1,8 @@
+# Add the required packages to the ks.cfg file
+for package in $AZ_REQUIRED_PACKAGES; do
+    sed -i "/^\x25include \x2ftmp\x2fvirtualmachine.*/i $package" $HOME_DIR/$ISO_EXTRACT_DIR/ks.cfg
+done
+
 # Disable service after first start
 echo "systemctl disable bootstrap_install" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
 
@@ -7,7 +12,12 @@ echo "curl -o cef_installer.py https://raw.githubusercontent.com/Azure/Azure-Sen
 # Enable listening for syslog in UDP 514 and TLS syslog in TCP 6514
 echo "firewall-cmd --permanent --add-service=syslog" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
 echo "firewall-cmd --permanent --add-service=syslog-tls" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
+echo "firewall-cmd --permanent --add-service=rsh" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
+echo "firewall-cmd --permanent --add-port=25226/tcp" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
 echo "firewall-cmd --reload" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
+
+# Allow the MMA through SELinux
+echo "semanage port -a -t syslogd_port_t -p tcp 25226" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
 
 # Disable first init service after finishing and remove script with data
 echo "systemctl disable bootstrap_install" >> $HOME_DIR/$ISO_EXTRACT_DIR/scripts/post_installation.sh
